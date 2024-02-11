557e7a3edbe2f4600222c2cd48559af94776336f
Suraj Sutar <151071295+Suryaa-Dev@users.noreply.github.com>
2024-02-11 21:19:05 +0530

    Update app.js
    Same as update in index.html

--- app.js
+++ app.js
@@ -5,18 +5,12 @@ function addFuntionalityTo(tableNumber){
         return quantity * rate;
     }
 
-    document.querySelector(`.table${tableNumber} .add-list`).addEventListener("click", ()=>{
+    document.querySelector(`.table${tableNumber} .right`).addEventListener("click", ()=>{
         let newList = document.createElement("div");
         newList.classList = "list";
         newList.innerHTML = `
             <select class="item_name"> 
-                <option value="0">Select the Option</option>  
-                <option value="20">Dabeli</option>  
-                <option value="60">Pavbhaji</option>  
-                <option value="20">Extra Pav(Jodi)</option>
-                <option value="10">Extra Pav(Single)</option>  
-                <option value="80">Special Pavbhaji</option>
-                <option value="70">Masala Pav</option>   
+                <option value="0">Select the Option</option>     
                 <optgroup label="Manchurian">   
                     <option value="50">Manchurian(Half)</option>       
                     <option value="100">Manchurian(Full)</option>
@@ -58,9 +52,6 @@ function addFuntionalityTo(tableNumber){
                     <option value="80">Soup (Full)</option> 
                     <option value="100">Chinese Bhel(Full)</option>
                 </optgroup>
-                <option value="10">Cold Drink(10)</option>
-                <option value="20">Cold Drink(20)</option>
-                <option value="10">Cold Drink(45)</option>
             </select>
             <p class="item_rate">0</p>
             <input type="text" inputmode="numeric" class="item_quantity">
@@ -68,7 +59,45 @@ function addFuntionalityTo(tableNumber){
             <button class="remove-btn"><img src="./imgs/remove.png"></button>`;
 
         container.appendChild(newList);
+        updatingList(newList);
+        
+        generateBill(tableNumber);
+    });
 
+    document.querySelector(`.table${tableNumber} .left`).addEventListener("click", ()=>{
+        let newList = document.createElement("div");
+        newList.classList = "list";
+        newList.innerHTML = `
+            <select class="item_name"> 
+                <option value="0">Select the Option</option>  
+                <option value="20">Dabeli</option>  
+                <option value="100">French Fries</option>
+                <option value="60">Pavbhaji</option>  
+                <option value="20">Extra Pav(Jodi)</option>
+                <option value="10">Extra Pav(Single)</option>  
+                <option value="80">Special Pavbhaji</option>
+                <option value="80">Kolhapuri Pavbhaji</option>
+                <option value="70">Masala Pav</option>
+
+                <option value="10">Cold Drink(10)</option>
+                <option value="20">Cold Drink(20)</option>
+                <option value="25">Cold Drink(25)</option>
+                <option value="40">Cold Drink(45)</option>
+                <option value="15">Ice Cream(15)</option>
+                <option value="20">Ice Cream(20)</option>
+                <option value="25">Ice Cream(25)</option>
+                <option value="30">Ice Cream(30)</option>
+                <option value="35">Ice Cream(35)</option>
+                <option value="40">Ice Cream(40)</option>
+                <option value="45">Ice Cream(45)</option>
+                <option value="50">Ice Cream(50)</option>
+            </select>
+            <p class="item_rate">0</p>
+            <input type="text" inputmode="numeric" class="item_quantity">
+            <p class="item_amount">0</p>
+            <button class="remove-btn"><img src="./imgs/remove.png"></button>`;
+
+        container.appendChild(newList);
         updatingList(newList);
         
         generateBill(tableNumber);
@@ -88,6 +117,7 @@ function addFuntionalityTo(tableNumber){
 
         itemQuantityInput.addEventListener('input', () => {
             calculateAndUpdateAmount();
+            generateBill(tableNumber);
         });
 
         function calculateAndUpdateAmount() {
@@ -117,14 +147,14 @@ function addFuntionalityTo(tableNumber){
     let heightManipul = document.querySelector(`.table${tableNumber} .heightManipulation`)
 
     document.querySelector(`.table${tableNumber} .final img`).addEventListener("click", ()=>{    
-        if(heightofTable.style.height == "100%" || heightofTable.style.height == ""){
-            heightofTable.style.height = 60 + "px"; 
-            heightofTable.style.backgroundColor = "#8f45ff";
-            heightManipul.style.display = "none";
-        }else{
+        if(heightofTable.style.height == "60px" || heightofTable.style.height == ""){
             heightofTable.style.height = 100 + "%";
             heightofTable.style.backgroundColor = "White";
             heightManipul.style.display = "block";
+        }else{
+            heightofTable.style.height = 60 + "px"; 
+            heightofTable.style.backgroundColor = "#8f45ff";
+            heightManipul.style.display = "none";
         }
     })
 }
@@ -132,8 +162,6 @@ function addFuntionalityTo(tableNumber){
 for(let tables = 1; tables<=9; tables++){
     addFuntionalityTo(tables);
 }
-
-
 
 
 
